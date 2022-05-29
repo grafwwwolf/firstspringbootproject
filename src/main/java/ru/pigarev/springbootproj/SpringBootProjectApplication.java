637f08b1f3f27e5f90ffb14f054e9dc3c4141380
Stanislav <grafwwwolf@yandex.ru>
2022-05-29 18:58:20 +0300

    1

--- src/main/java/ru/pigarev/springbootproj/SpringBootProjectApplication.java
+++ src/main/java/ru/pigarev/springbootproj/SpringBootProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootprojectApplication {
+public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootprojectApplication.class, args);
+		SpringApplication.run(SpringBootProjectApplication.class, args);
 	}
 
 }
